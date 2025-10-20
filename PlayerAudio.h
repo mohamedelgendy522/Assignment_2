0176ad332fd2d9a300284fafdee6ade0c1d20ae0
Ahmed Bakr <ab163545@gmail.com>
2025-10-20 18:49:17 +0300

    Initial Commit

---
+++ PlayerAudio.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <JuceHeader.h>
+class PlayerAudio {
+    bool isMuted = false;
+public:
+    PlayerAudio();
+    ~PlayerAudio();
+    void prepareToPlay(int samplesPerBlockExpected, double sampleRate) ;
+    void getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill) ;
+    void releaseResources() ;
+    bool loadFile(const juce::File& file);
+    void mute();
+    void play();
+    void stop();
+    void setGain( float gain);
+    void setPosition( float pos);
+    double getPosition()  ;
+    double getLength()  ;
+
+
+private:
+    juce::AudioFormatManager formatManager;
+    std::unique_ptr<juce::AudioFormatReaderSource> readerSource;
+    juce::AudioTransportSource transportSource;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(PlayerAudio)
+};
---
+++ PlayerGUI.cpp
@@ -0,0 +1,89 @@
+#include "PlayerGUI.h"
+#include <JuceHeader.h>
+PlayerGUI::PlayerGUI()
+{
+    for (auto* btn : { &loadButton, &restartButton , & stopButton , &MuteButton })
+    {
+        btn->addListener(this);
+        addAndMakeVisible(btn);
+    }
+
+    // Volume slider
+    volumeSlider.setRange(0.0, 1.0, 0.01);
+    volumeSlider.setValue(0.5);
+    volumeSlider.addListener(this);
+    addAndMakeVisible(volumeSlider);
+}
+
+PlayerGUI::~PlayerGUI(){}
+
+void PlayerGUI::prepareToPlay(int samplesPerBlockExpected, double sampleRate)
+{
+    playerAudio.prepareToPlay(samplesPerBlockExpected, sampleRate);
+}
+
+void PlayerGUI::getNextAudioBlock(const juce::AudioSourceChannelInfo &bufferToFill)
+{
+    playerAudio.getNextAudioBlock(bufferToFill);
+}
+void PlayerGUI::releaseResources()
+{
+    playerAudio.releaseResources();
+}
+void PlayerGUI::resized() {
+    int y = 20;
+    loadButton.setBounds(20, y, 100, 40);
+    restartButton.setBounds(140, y, 80, 40);
+    stopButton.setBounds(240, y, 80, 40);
+    volumeSlider.setBounds(20, 100, getWidth() - 40, 30);
+    MuteButton.setBounds(340, y, 80, 40);
+
+}
+
+void PlayerGUI::buttonClicked(juce::Button* button)
+{
+    if (button == &loadButton)
+    {
+        juce::FileChooser chooser("Select audio files...",
+            juce::File{},
+            "*.wav;*.mp3");
+
+        fileChooser = std::make_unique<juce::FileChooser>(
+            "Select an audio file...",
+            juce::File{},
+            "*.wav;*.mp3");
+
+        fileChooser->launchAsync(
+            juce::FileBrowserComponent::openMode | juce::FileBrowserComponent::canSelectFiles,
+            [this](const juce::FileChooser& fc)
+            {
+                auto file = fc.getResult();
+                if (file.existsAsFile())
+                    playerAudio.loadFile(file);
+            });
+    }
+
+    if (button == &restartButton)
+    {
+        playerAudio.play();
+    }
+
+    if (button == &stopButton)
+    {
+        playerAudio.stop();
+        playerAudio.setPosition(0.0);
+    }
+    if (button == &MuteButton) {
+        playerAudio.mute();
+    }
+
+}
+void PlayerGUI::paint(juce::Graphics& g)
+{}
+void PlayerGUI::sliderValueChanged(juce::Slider* slider)
+{
+    if (slider == &volumeSlider)
+        playerAudio.setGain((float)volumeSlider.getValue());
+}
+
+
