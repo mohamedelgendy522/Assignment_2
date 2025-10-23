071d477770f7f087e88ac9260979c3c5e7752839
Mohamed Mahmoud Ahmed Mahmoud ELGendy <mm6920@o365.fayoum.edu.eg>
2025-10-23 07:59:40 +0300

    Reorder and format PlayerAudio class methods

--- PlayerAudio.h
+++ PlayerAudio.h
@@ -4,17 +4,20 @@ class PlayerAudio {
 public:
     PlayerAudio();
     ~PlayerAudio();
-    void prepareToPlay(int samplesPerBlockExpected, double sampleRate) ;
-    void getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill) ;
-    void releaseResources() ;
+    void prepareToPlay(int samplesPerBlockExpected, double sampleRate);
+    void getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill);
+    void releaseResources();
     bool loadFile(const juce::File& file);
+
     void play();
     void stop();
-    void setGain( float gain);
-    void setPosition( float pos);
-    double getPosition()  ;
-    double getLength()  ;
+    void setGain(float gain);
+    void setPosition(float pos);
+    double getPosition();
+    double getLength();
+	void setRepeat(bool shouldRepeat);
 private:
+    bool Repeat = false;
     juce::AudioFormatManager formatManager;
     std::unique_ptr<juce::AudioFormatReaderSource> readerSource;
     juce::AudioTransportSource transportSource;
