02fd3fb59316c7077d204cd87f2a9cef48ba1f99
Ahmed Bakr <ab163545@gmail.com>
2025-10-30 20:29:29 +0300

    Update PlayerAudio.h

--- PlayerAudio.h
+++ PlayerAudio.h
@@ -5,28 +5,29 @@ public:
     PlayerAudio();
     ~PlayerAudio();
     void prepareToPlay(int samplesPerBlockExpected, double sampleRate);
-    void getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill);
+    void getNextAudioBlock(const juce::AudioSourceChannelInfo&
+    bufferToFill);
     void releaseResources();
     bool loadFile(const juce::File& file);
-	void setMuted(bool shouldMute);
+    void setMuted(bool shouldMute);
     void play();
+    void stop();
     void pause();
     void restart();
     void end();
     void start();
-    void stop();
     void setGain(float gain);
     void setPosition(float pos);
     double getPosition();
     double getLength();
-	void setRepeat(bool shouldRepeat);
+    void setRepeat(bool shouldRepeat);
+    void setSpeed( float s);
 private:
     bool Repeat = false;
-	float lastGain = 1.0f;
     bool isMuted = false;
     juce::AudioFormatManager formatManager;
     std::unique_ptr<juce::AudioFormatReaderSource> readerSource;
     juce::AudioTransportSource transportSource;
-
+    juce::ResamplingAudioSource resampleSource { &transportSource, false };
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(PlayerAudio)
-};
+    };
