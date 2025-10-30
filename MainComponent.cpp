ac59b441785491ddbdfeccac937fd449b9d928a9
Ahmed Bakr <ab163545@gmail.com>
2025-10-30 20:28:20 +0300

    Update MainComponent.cpp

--- MainComponent.cpp
+++ MainComponent.cpp
@@ -1,37 +1,32 @@
 #include "MainComponent.h"
-
-MainComponent::MainComponent()
+MainComponent::MainComponent() : player1(playerAudio)
 {
-
     addAndMakeVisible(player1);
-
     player1.onMuteChanged = [this] { updateMute(); };
-    setSize(600, 400);
+    setSize(650, 450);
     setAudioChannels(0, 2);
 }
-
 MainComponent::~MainComponent()
 {
     shutdownAudio();
 }
-
-void MainComponent::prepareToPlay(int samplesPerBlockExpected, double sampleRate)
+void MainComponent::prepareToPlay(int samplesPerBlockExpected, double
+sampleRate)
 {
     player1.prepareToPlay(samplesPerBlockExpected, sampleRate);
 }
-
-void MainComponent::getNextAudioBlock(const juce::AudioSourceChannelInfo &bufferToFill)
+void MainComponent::getNextAudioBlock(const juce::AudioSourceChannelInfo&
+bufferToFill)
 {
     bufferToFill.clearActiveBufferRegion();
     player1.getNextAudioBlock(bufferToFill);
 }
-
 void MainComponent::releaseResources()
 {
     player1.releaseResources();
 }
 void MainComponent::resized() {
-    player1.setBounds(20, 20 , getWidth() - 40, 270);
+    player1.setBounds(getLocalBounds());
 }
 void MainComponent::updateMute()
 {
@@ -39,9 +34,3 @@ void MainComponent::updateMute()
     playerAudio.setMuted(isMuted);
     repaint();
 }
-
-
-
-
-
-
