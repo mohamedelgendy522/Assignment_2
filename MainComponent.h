4c4280ebbe685f33198d09177191c7ab2b64c30a
Ahmed Bakr <ab163545@gmail.com>
2025-12-28 01:18:58 +0200

    Add paint and updateCrossfade methods to MainComponent

--- MainComponent.h
+++ MainComponent.h
@@ -11,11 +11,13 @@ public:
     void getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill) override;
     void releaseResources() override;
     void resized() override;
+    void paint(juce::Graphics& g) override;
 
 
 
 private:
     void updateMixState();
+    void updateCrossfade();
     bool isMuted = false;
 
     PlayerAudio playerAudio1;
@@ -32,6 +34,7 @@ private:
     // Crossfade slider
     juce::Slider crossfadeSlider;
     juce::Label crossfadeLabel;
+    juce::Label leftLabel, rightLabel;
 
     //void loadTrack(const juce::File& file);
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainComponent)
