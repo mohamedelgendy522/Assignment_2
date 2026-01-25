130a95b7170b0e86d2b0b00b4371f3f5e49a3464
Ahmed Bakr <ab163545@gmail.com>
2026-01-26 01:54:19 +0200

    Add ModernLookAndFeel to MainComponent

--- MainComponent.h
+++ MainComponent.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <JuceHeader.h>
 #include "PlayerGUI.h"
+#include "StyleSheet.h"
 
 class MainComponent : public juce::AudioAppComponent {
 public:
@@ -25,6 +26,8 @@ private:
 
     PlayerGUI player1{ playerAudio1 };
     PlayerGUI player2{ playerAudio2 };
+
+    ModernLookAndFeel modernLook;
 
     // mixer and UI control
     juce::MixerAudioSource mixer;
